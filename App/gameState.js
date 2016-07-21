f88ec407533761e2eb7a8f0f3c876829387c0704
Evan Moncuso <evan.moncuso@gmail.com>
2016-07-21 12:34:42 -0700

    Describe purpose of app/game states

--- App/gameState.js
+++ App/gameState.js
@@ -1,4 +1,15 @@
 const gameState = {
+
 };
 
 module.exports = gameState;
+
+/*
+==========================
+Game State
+==========================
+gameState is where all the game specific information is held
+
+At any moment we can request new info for a game, so this will be updated and overwritten whenever the user switches to a new gameRoutes
+==========================
+*/
